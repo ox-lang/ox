3634b9bfea643d7c3e22869e3f33bf7c4de3cdba
arrdem <me@arrdem.com>
2014-09-04 13:47:52 -0500

    Fix groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oxlang "0.1.0-SNAPSHOT"
+(defproject org.oxlang/oxlang "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIX/X11 license"
