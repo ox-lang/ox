a3b65e29804f2ae062dc374b327bacb408ff0bf2
arrdem <me@arrdem.com>
2014-09-04 13:48:19 -0500

    Docs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.oxlang/oxlang "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An experiment in implementing Oxlang"
+  :url "http://github.com/oxlang/oxlang"
   :license {:name "MIX/X11 license"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
