63eedd2acc404c55b07c8358b2b9014fa3d0d184
arrdem <me@arrdem.com>
2014-09-10 14:46:38 -0500

    Add some test coverage for characters

--- test/oxlang/reader_test.clj
+++ test/oxlang/reader_test.clj
@@ -27,6 +27,16 @@
       (doseq [i ["a" "a.b" "a.b/c" "a.b//" "/"]]
         (is (= (p i) (with-prefix i)))))))
 
+(deftest character-tests
+  (let [chars (map (partial str "\\")
+                   (concat (map char (range 97 123))
+                           ["newline" "space" "tab" "backspace" "formfeed" "return"]
+                           ["u000" "u00" "u0"]))]
+    (testing "Character consumer alone"
+      (let [p (insta/parser grammar :start :character)]
+        (doseq [c chars]
+          (is (p c)))))))
+
 (deftest keyword-tests)
 
 (deftest vector-tests
