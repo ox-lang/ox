4536a22d057e71be49c78151e2b13019cebc659a
arrdem <me@arrdem.com>
2014-09-27 20:24:11 -0500

    merge

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.typed "0.2.68"]
-                 [me.arrdem/detritus "0.1.0"]
+                 [me.arrdem/detritus "0.2.0-SNAPSHOT"]
                  [instaparse/instaparse "1.3.4"]])
