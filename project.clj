3ca13210947f93e4a4367828b538c437b94357a8
arrdem <me@arrdem.com>
2014-10-24 23:44:45 -0500

    test.check is good stuff

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/oxlang/oxlang"
   :license {:name "MIX/X11 license"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/test.check "0.5.9"]])
