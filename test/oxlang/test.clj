29955c1e0e00f016f3ca90779a33cd359d392aaa
arrdem <me@arrdem.com>
2014-10-25 02:56:53 -0500

    Add oxlang.test

---
+++ test/oxlang/test.clj
@@ -0,0 +1,13 @@
+(ns oxlang.test
+  (:require [oxlang.test :refer :all]
+            [clojure.test :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
+
+(defmacro distinct-n-tuple [a n]
+  `(gen/such-that
+       (fn [s#]
+         (= (count s#)
+            (count (into #{} s#))))
+     (gen/tuple ~@(repeat n a))))
