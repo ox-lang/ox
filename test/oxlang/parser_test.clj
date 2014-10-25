144e812d5565135c6b2488fdd64deb12f1c916ab
arrdem <me@arrdem.com>
2014-10-25 00:52:45 -0500

    Test conc

--- test/oxlang/parser_test.clj
+++ test/oxlang/parser_test.clj
@@ -57,3 +57,32 @@
       (and (success? r)
            (= (:dat r)
               (t a))))))
+
+;; Test :conc
+;;--------------------------------------------------------------------
+(defspec conc-matches
+  (prop/for-all [[a b] (distinct-n-tuple gen/char 2)]
+    (let [g {:a     {:op  :term,
+                     :val a}
+             :b     {:op  :term,
+                     :val b}
+             :entry {:op   :conc,
+                     :body [:a, :b]}}
+          p (partial parse g :entry)]
+      (success? (p [a b])))))
+
+(defspec conc-transform-ok
+  (prop/for-all [[a b] (distinct-n-tuple gen/char 2)]
+    (let [t  (fn [x] (/ (max (long x) 1) 2))
+          t' (partial map t)
+          g  {:a     {:op  :term,
+                      :val a}
+              :b     {:op  :term,
+                      :val b}
+              :entry {:op   :conc,
+                      :body [:a, :b]
+                      :transform t'}}
+          r  (parse g :entry [a b])]
+      (and (success? r)
+           (= (:dat r)
+              (t' [a b]))))))
