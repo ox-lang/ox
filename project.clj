ed83a8a19bb2c15c71c1df3afb5b8cbcd129b8da
arrdem <me@arrdem.com>
2014-10-25 04:19:50 -0500

    Add radagast whitelist

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/oxlang/oxlang"
   :license {:name "MIX/X11 license"
             :url "http://opensource.org/licenses/MIT"}
+  :whitelist #"oxlang.*"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.check "0.5.9"]])
