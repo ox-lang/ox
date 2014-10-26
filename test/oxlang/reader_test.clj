43f4b301146fba16430d7db78e69efa9e824fe42
arrdem <me@arrdem.com>
2014-10-25 21:06:47 -0500

    Add tests for keywords

--- test/oxlang/reader_test.clj
+++ test/oxlang/reader_test.clj
@@ -22,3 +22,21 @@
   (are [s] (let [r (parse symbol :symbol s)]
              (failure? r))
        "0" ":" ":a" "::a" "{" "}" "(" ")" " " "\n" "\t"))
+
+(deftest keyword-tests
+  (are [s v] (let [r (parse keyword :keyword s)]
+               (and (success? r)
+                    (= v (:dat r))))
+       
+       ":a"            [:unqualified "a"]
+       "::b"           [:unqualified "b"]
+       ":牛"           [:unqualified "牛"]
+       "://"           [:unqualified "//"]
+       ":ox.core/conj" [:qualified   "ox.core/conj"]
+       ":ox.core/->"   [:qualified   "ox.core/->"]
+       ":ox.core/->>"  [:qualified   "ox.core/->>"]
+       ":ox.core/牛"   [:qualified   "ox.core/牛"])
+
+  (are [s] (let [r (parse keyword :keyword s)]
+             (failure? r))
+       "0" "{" "}" "(" ")" " " "\n" "\t" "a" "a/b" "//"))
