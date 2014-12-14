aecefa6302c5b769193a708ad00419bda1f8f89d
arrdem <me@arrdem.com>
2014-12-14 02:54:39 -0600

    Add ox.lang.Util

--- src/main/java/ox/lang/Util.java
+++ src/main/java/ox/lang/Util.java
@@ -3,6 +3,12 @@ import java.lang.IllegalArgumentException;
 import java.lang.Character;
 
 public class Util {
+    /* This function serves as a helper for constructing Unicode
+     * characters from their hex representations, a task that would
+     * otherwise be quite the chore from ox itself.
+     *
+     * Totally not lifted from clojure.lang.LispReader. Totally. 
+     */ 
     public static Character readUnicodeChar(String token, int offset, int length, int base) {
         if(token.length() != offset + length)
             throw new IllegalArgumentException("Invalid unicode character: \\" + token);
