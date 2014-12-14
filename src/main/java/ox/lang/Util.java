b9847731a3e3a347ec76862e185d7ade49490c51
arrdem <me@arrdem.com>
2014-12-14 03:01:15 -0600

    DOCS DOCS DOCS

--- src/main/java/ox/lang/Util.java
+++ src/main/java/ox/lang/Util.java
@@ -2,6 +2,17 @@ package ox.lang;
 import java.lang.IllegalArgumentException;
 import java.lang.Character;
 
+/* This class serves as a holding tank for snippets of Java code which
+ * are both stateless, and serve as helpers in interacting with the ox
+ * implementation and datastructures.
+ *
+ * As a helper class, this class should have no instance variables or
+ * methods. Constants and statics are OK.
+ *
+ * Roughly analogous to clojure.lang.Util, but this is the preferred
+ * place to put stuff rather than the alternate.
+ */ 
+
 public class Util {
     /* This function serves as a helper for constructing Unicode
      * characters from their hex representations, a task that would
