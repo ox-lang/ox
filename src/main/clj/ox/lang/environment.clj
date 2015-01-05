6d31691f58b126ccc1f3d9e48041d6c521145951
arrdem <me@arrdem.com>
2015-01-05 12:46:00 -0600

    Implement a trivial interpreting eval & envs

--- src/main/clj/ox/lang/environment.clj
+++ src/main/clj/ox/lang/environment.clj
@@ -1,4 +1,5 @@
-(ns ox.lang.environment)
+(ns ox.lang.environment
+  (:refer-clojure :exclude [resolve]))
 
 (defn env?
   "FIXME: quick and dirty predicate"
@@ -12,22 +13,47 @@
   (and (env? x)
        (= :env/ns (first x))))
 
+(defn alias?
+  "FIXME: quick an dirty predicate"
+  [x]
+  (and (vector? x)
+       (#{:binding/alias} (first x))))
+
+(defn value?
+  "FIXME: quick and dirty predicate"
+  [x]
+  (and (vector? x)
+       (#{:binding/value} (first x))))
+
 (defn make-environment
   "λ [] → Env
 
   Returns the empty environment. Analyzing or evaluating any namespace must
   start with the empty environment."
-  []
+  [ns]
   [:env/ns
-   {:parent            nil ; link to parent environment
+   {:ns                ns  ; symbol naming current namespace
+    :parent            nil ; link to parent environment
 
     :loaded-namespaces {}  ; map from symbols to the definition environment
 
     :imports           #{} ; set of imported classes
 
-    :bindings          {}  ; map from qualified and unqualified
-                           ; symbols to a binding descriptor.
-    }])
+    ;; map from qualified and unqualified
+    ;; symbols to a binding descriptor.
+    :bindings          {'+       [:binding/value (list 'fn* (list (list 'x 'y) (list 'invoke #'clojure.core/+ 'x 'y)))]
+                        'println [:binding/value (list 'fn* (list (list 'x)    (list 'invoke #'clojure.core/println 'x)))]
+                        'apply   [:binding/special 'apply]
+                        'def*    [:binding/special 'def*]
+                        'do*     [:binding/special 'do*]
+                        'fn*     [:binding/special 'fn*]
+                        'if*     [:binding/special 'if*]
+                        'let*    [:binding/special 'let*]
+                        'letrc*  [:binding/special 'letrc*]
+                        'ns      [:binding/alias   'ox.lang.bootstrap/ns]
+                        'ns*     [:binding/special 'ns*]
+                        'quote   [:binding/special 'quote]
+                        'invoke  [:binding/special 'invoke]}}])
 
 (defn make-local-environment
   "λ [Env] → Env
@@ -44,8 +70,52 @@
 
   Returns a new environment where the specified symbol is bound to the given
   form value. Used for installing defs into an environment."
-  [env symbol value]
-  (assert false "Unimplemented"))
+  [env sym value]
+  {:pre [(ns? env)]}
+  (let [ns   (-> env second :ns)
+        qsym (symbol (name ns) (name sym))]
+    (-> env
+        (assoc-in [1 :bindings sym]  ^:no-export [:binding/alias qsym])
+        (assoc-in [1 :bindings qsym] [:binding/value value]))))
+
+(defn get-entry
+  [env symbol]
+  (if-let [val (-> env second (get :bindings) (get symbol))] val
+          (if-let [parent (:parent (second env))]
+            (get-entry parent symbol)
+            (assert false (str symbol " is not bound in any enclosing scope!")))))
+
+(defn resolve
+  "λ [Env, Symbol] → Maybe[Symbol]
+
+  Resolves the given symbol in the current environment."
+  [env sym]
+  (let [entry (get-entry env sym)]
+    (if (alias? entry)
+      (recur env (second entry))
+      sym)))
+
+(defn get-value
+  "λ [Env, Symbol] → Value
+
+  Returns the value of the given symbol in the specified
+  environment."
+  [env symbol]
+  (let [entry (get-entry env symbol)]
+    (assert (value? entry))
+    (second entry)))
+
+(defn special?
+  "FIXME: quick and dirty predicate"
+  [env symbol]
+  (let [x (->> symbol
+               (resolve env)   ;; must return a valid new symbol or fail
+               (get-entry env))]
+    (boolean
+     (and (vector? x)
+          (#{:binding/special} (first x))))))
+
+
 
 (defn get-meta
   "λ [Env, Symbol] → Map
@@ -55,22 +125,46 @@
   metadata, fails if the symbol is not bound in the given
   environment."
   [env symbol]
-  (assert false "Unimplemented"))
+  (meta (get-value env symbol)))
+
+(defn set-meta
+  "λ [Env, Symbol, Map] → Env
+
+  Returns an updated environment where the metadata of the given
+  symbol has been altered to equal the argument map."
+  [env symbol meta]
+  {:pre [(do (get-entry env symbol) true)]}
+  (let [s        (resolve env symbol)
+        bindings (-> env second (get :bindings))]
+    (if-let [[k v] (find bindings s)]
+      (assoc-in env [1 :bindings s] (with-meta v meta))
+      (assoc-in env [1 :parent]     (set-meta (-> env second :parent)
+                                              symbol meta)))))
+
+
 
 (defn push-locals
-  "λ [Env, (Seq (Symbol . Form))] → Env
+  "λ [Env, {[Symbol Form]}] → Env
 
   Pushes local bindings, returning a new environment with the pushed
   local bindings."
-  [env symbol form]
-  (assert false "Unimplemented"))
+  [env bindings]
+  [:env/locals {:parent env
+                :bindings
+                (->> (for [[k v] bindings]
+                       [k [:binding/value v]])
+                     (into {}))}])
 
 (defn pop-locals
   "λ [Env] → Env
 
   Pops the last set of pushed local bindings, returning the parent environment."
   [env]
-  (assert false "Unimplemented"))
+  {:pre [(vector? env)
+         (= :env/local (first env))]}
+  (-> env second :parent))
+
+
 
 (defn macro?
   "λ [Env, Symbol] → Bool
@@ -78,4 +172,4 @@
   Indicates whether a given symbol is bound to a macro in the given
   environment."
   [env symbol]
-  (assert false "Unimplemented"))
+  (:macro (get-meta env (resolve env symbol))))
--- src/main/clj/ox/lang/evaluator.clj
+++ src/main/clj/ox/lang/evaluator.clj
@@ -1,5 +1,6 @@
 (ns ox.lang.evaluator
-  (:refer-clojure :exclude [eval macroexpand-1 macroexpand]))
+  (:refer-clojure :exclude [eval apply macroexpand-1 macroexpand])
+  (:require [ox.lang.environment :as env]))
 
 (declare eval)
 
@@ -8,11 +9,11 @@
   form given that it has already been recursively read-eval'd.
 
   (let* ((env ...))
-    (read-eval env '(read-eval FORM))
+  (read-eval env '(read-eval FORM))
   => (eval env FORM)
 
   forms which do not start with read-eval are returned unmodified."
-  [env tree]
+  [eval env tree]
   (if (= 'read-eval (first tree))
     (eval env (second tree))
     tree))
@@ -25,19 +26,140 @@
 
   When reading structures, users are required to read-eval their forms before
   macroexpanding or evaluating them."
-  [env tree]
+  [eval env tree]
   (if (list? tree)
     (->> tree
-         (map (partial read-eval env))
-         (read-eval-1 env))
+         (map (partial read-eval eval env))
+         (read-eval-1 eval env))
     tree))
 
 
 
 (defn macroexpand-1
-  [env tree])
+  [eval env tree]
+  (if (and (list? tree)
+           (:macro (env/get-meta (first tree))))
+    (eval env tree)
+    tree))
+
+(defn fix [f x]
+  (loop [x x]
+    (let [x' (f x)]
+      (if-not (= x x')
+        (recur x')
+        x))))
 
 (defn macroexpand
   "Walks a previously read-eval'd tree, expanding macros occuring in the tree
-  via macroexpand-1. Support is provided for symbol macros."
-  [env tree])
+  via macroexpand-1. Children are expanded first until they reach a fixed point,
+  then the parent is expanded until it fixes as well."
+  [eval env tree]
+  (if (list? tree)
+    (->> tree
+         (map (partial macroexpand eval env))
+         (fix (partial macroexpand-1 eval env)))
+    tree))
+
+
+
+(defn compute-fn-bindings [args vals]
+  (loop [acc {}
+         args args
+         vals vals]
+    (let [[arg & args'] args
+          [val & vals'] vals]
+      (if-not (empty? args)
+        (if (= arg '&)
+          (assoc acc (first args') vals)
+          (recur (assoc acc arg val) args' vals'))
+        acc))))
+
+(declare interpreting-eval)
+
+(defn interpreting-eval-1 [env form]
+  (let [-e (comp second (partial interpreting-eval env))]
+    (cond (or (list? form)
+              (instance? clojure.lang.Cons form))
+          ,,(let [[f & args] form]
+              (case f
+                (apply)
+                ,,(let [[lambda & args]   args
+                        _                 (assert (= 'fn* (first lambda)) "Not applying a fn*!")
+                        bodies            (->> lambda rest
+                                               (sort-by #(count (first %1))))
+                        app-args          (concat (map -e (butlast args))
+                                                  (-e (last args)))
+                        body              (or (loop [bodies bodies]
+                                                (let [[[fn-args & forms :as b] & bodies] bodies]
+                                                  (if (= (count app-args)
+                                                         (count fn-args))
+                                                    b
+                                                    (when-not (empty? bodies)
+                                                      (recur bodies)))))
+                                              (last bodies))
+                        [fn-args & forms] body]
+                    #(interpreting-eval
+                      (->> (compute-fn-bindings fn-args app-args)
+                           (env/push-locals env))
+                      `(~'do* ~@forms)))
+
+                (def*)
+                ,,(let [[s m v & more] args]
+                    (assert (symbol? s) "Not a binding symbol!")
+                    (assert (map? m) "Metadata not a map!")
+                    (assert (empty? more) "More args to def than expected!")
+                    [(-> env
+                         (env/inter s v)
+                         (env/set-meta s m))
+                     s])
+
+                (do*)
+                ,,(do (doseq [f (butlast args)]
+                        (-e f))
+                      #(interpreting-eval env (last args)))
+
+                (fn*)
+                ,,[env form]
+
+                (if*)
+                ,,[env
+                   (let [[pred left right & more] args]
+                     (-e (if (-e pred) left right)))]
+
+                (let*)
+                ,,(let [[bindings & forms] args
+                        bindings           (->> (for [[k v] bindings]
+                                                  [k (-e v)])
+                                                (into {}))
+                        [_ res]            (interpreting-eval
+                                            (env/push-locals env bindings)
+                                            `(~'do* ~@forms))]
+                    [env res])
+
+                (letrc*)
+                ,,(assert false "letrc isn't supported yet™")
+
+                (quote)
+                ,,[env (second form)]
+
+                (invoke)
+                ,,(let [[f & args] args]
+                    [env (clojure.core/apply f (map -e args))])
+
+                #(interpreting-eval env `(~'apply ~@(map -e form) '()))))
+
+          (symbol? form)
+          ,,(if (env/special? env form)
+              [env form] ;; self-evaluate
+              [env (->> form
+                        (env/resolve env)
+                        (env/get-value env))])
+
+          :else ;; everything else is self-evaluating
+          ,,[env form])))
+
+(defn interpreting-eval
+  "Walks a previously read-eval'd and macroexpand'd form, evaluating it by
+  interpretation. Returns a pair [env, result]."
+  [env form]
+  (trampoline interpreting-eval-1 env form))
