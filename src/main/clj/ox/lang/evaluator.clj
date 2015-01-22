7f6bbcd154cb2985f4098b8fc4ec3c6157a2e468
arrdem <me@arrdem.com>
2015-01-22 12:17:57 -0600

    [NO TESTS] WIP

--- src/main/clj/ox/lang/evaluator.clj
+++ src/main/clj/ox/lang/evaluator.clj
@@ -40,7 +40,8 @@
 (defn macroexpand-1
   [eval env tree]
   (if (and (list? tree)
-           (:macro (env/get-meta (first tree))))
+           (symbol? (first tree))
+           (true? (:macro (env/get-meta (first tree)))))
     (eval env tree)
     tree))
 
@@ -62,117 +63,24 @@
          (fix (partial macroexpand-1 eval env)))
     tree))
 
-
+(def mapl (comp list* map))
 
-(defn compute-fn-bindings [args vals]
-  (loop [acc  {}
-         args args
-         vals vals]
-    (let [[arg & args'] args
-          [val & vals'] vals]
-      (if-not (empty? args)
-        (if (= arg '&)
-          (assoc acc (first args') vals)
-          (recur (assoc acc arg val) args' vals'))
-        acc))))
-
-(declare interpreting-eval)
-
-(defn interpreting-eval-1 [env form]
-  ;; FIXME: environments should support options, add a eval-printing? option
-  ;; (println "[eval]" form)
-  (let [-e (comp second (partial interpreting-eval env))]
-    (cond (and (seq? form)
-               (not (vector? form))
-               (not (map? form)))
-          ,,(let [[f & args] form]
-              (case f
-                (apply*)
-                ,,(let [[lambda & args]   args
-                        _                 (assert (= 'fn* (first lambda)) "Not applying a fn*!")
-                        bodies            (->> lambda rest
-                                               (sort-by #(count (first %1))))
-                        app-args          (concat (mapv -e (butlast args))
-                                                  (-e (last args)))
-                        body              (or (loop [bodies bodies]
-                                                (let [[[fn-args & forms :as b] & bodies] bodies]
-                                                  (if (= (count app-args)
-                                                         (count fn-args))
-                                                    b
-                                                    (when-not (empty? bodies)
-                                                      (recur bodies)))))
-                                              (last bodies))
-                        [fn-args & forms] body]
-                    #(interpreting-eval
-                      (->> (compute-fn-bindings fn-args app-args)
-                           (env/push-locals env))
-                      `(~'do* ~@forms)))
-
-                (def*)
-                ,,(let [[s m v & more] args]
-                    (assert (symbol? s) "Not a binding symbol!")
-                    (assert (or (nil? m) (map? m)) "Metadata not a mapv!")
-                    (assert (empty? more) "More args to def than expected!")
-                    [(-> env
-                         (env/inter s v)
-                         (env/set-meta s m))
-                     s])
-
-                (do*)
-                ,,(do (doseq [f (butlast args)]
-                        (-e f))
-                      #(interpreting-eval env (last args)))
-
-                (fn*)
-                ,,[env form]
-
-                (if*)
-                ,,[env
-                   (let [[pred left right & more] args]
-                     (-e (if (-e pred) left right)))]
-
-                (let*)
-                ,,(let [[bindings & forms] args
-                        bindings           (->> (for [[k v] bindings]
-                                                  [k (-e v)])
-                                                (into {}))
-                        [_ res]            (interpreting-eval
-                                            (env/push-locals env bindings)
-                                            `(~'do* ~@forms))]
-                    [env res])
-
-                (letrc*)
-                ,,(assert false "letrc isn't supported yet™")
-
-                (list*)
-                ,,[env
-                   (concat (mapv -e (butlast args))
-                           (-e (last args)))]
-
-                (quote)
-                ,,[env (second form)]
-
-                (invoke)
-                ,,(let [[f & args] args]
-                    [env (clojure.core/apply f (mapv -e args))])
-
-                #(interpreting-eval env `(~'apply* ~(-e f) ~@args nil))))
-
-          (symbol? form)
-          ,,(if (env/special? env form)
-              [env form] ;; self-evaluate
-              [env (->> form
-                        (env/resolve env)
-                        (env/get-value env))])
-
-          :else ;; everything else is self-evaluating
-          ,,[env form])))
-
-(defn interpreting-eval
-  "Walks a previously read-eval'd and macroexpand'd form, evaluating it by
-  interpretation. Returns a pair [env, result]."
+(defn -analyze-form
   [env form]
-  (trampoline interpreting-eval-1 env form))
+  (cond (list? form)
+        ,,true
+  
+        (symbol? form)
+        ,,true
+
+        true
+        ,,true))
+
+(defn analyze-form
+  "Walks the given form qualifying all used globals (defs) and renaming locals
+  to an explicit SSA form."
+  [env form]
+  (trampoline (partial -analyze-form env) form))
 
 (defn eval-form
   "Analyzes, macroexpands and interprets a single form in the given
@@ -182,18 +90,3 @@
        (read-eval eval env)
        (macroexpand eval env)
        (eval env)))
-
-(defn read-eval [eval env form]
-  (try
-    (-> (eval-form (partial eval env)) first)
-    (catch Exception e
-      (.printStackTrace e)
-      env)))
-
-(defn interpreting-load
-  [path]
-  (->> (str path ".ox")
-       io/resource io/file slurp
-       parser/parse-file
-       (reduce (partial read-eval interpreting-eval)
-               (env/make-environment 'user))))
