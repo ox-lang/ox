3f7010bc789b8d0cfef180b159b09065a00951ec
arrdem <me@arrdem.com>
2015-04-15 01:36:19 -0500

    Remove tests of removed interpreting-eval

--- src/test/clj/ox/lang/evaluator_test.clj
+++ src/test/clj/ox/lang/evaluator_test.clj
@@ -6,17 +6,3 @@
 
 (deftest fix-test
   (is (= 6 (fix #(min 6 (inc %1)) 0))))
-
-(deftest eval-if-test
-  (let [env (env/make-environment 'user)]
-    (is (= [env 2]
-           (interpreting-eval
-            env '(if* false 1 2))))
-
-    (is (= [env 1]
-           (interpreting-eval
-            env '(if* '() 1 2))))
-
-    (is (= [env 1]
-           (interpreting-eval
-            env '(if* true 1 2))))))
