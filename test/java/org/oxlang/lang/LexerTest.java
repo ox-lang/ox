32de4a9f53a0e45fecbbef549315ae91fef00859
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-05-11 15:53:14 -0700

    Explain what output was produced

--- test/java/org/oxlang/lang/LexerTest.java
+++ test/java/org/oxlang/lang/LexerTest.java
@@ -1,11 +1,16 @@
 package org.oxlang.lang;
 
+import java.io.StringReader;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.antlr.v4.runtime.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LexerTest {
 
@@ -23,60 +28,54 @@ public class LexerTest {
     return lexToken(text).getType();
   }
 
+  static void assertToken(String text, int type) {
+    List<Token> tokens = lexString(text);
+    if (tokens != null) {
+
+      boolean flag = tokens.size() == 1;
+      assertTrue(
+          String.format(
+              "Expected '%s' to lex to a single token (%d), got %d (%s)",
+              text, type, tokens.size(),
+              tokens.stream()
+                  .map(token -> Integer.toString(token.getType()))
+                  .collect(Collectors.joining(", "))),
+          flag);
+
+      if (flag) {
+        Token t = tokens.get(0);
+
+        flag = t.getType() == type;
+        assertTrue(
+            String.format(
+              "Expected '%s' to lext to token %d, got %d",
+              text, type, t.getType()),
+            flag);
+      }
+    }
+  }
+
   @Test
   public void testLexSymbol() throws Exception {
-    assertEquals(lexTokenType("f"),
-        OxlangLexer.SYMBOL);
+    String[] examples = {"foo", "foo/bar", "/", "+", "-"};
 
-    assertEquals(lexTokenType("foo"),
-        OxlangLexer.SYMBOL);
-
-    assertEquals(lexTokenType("foo/bar"),
-        OxlangLexer.SYMBOL);
-
-    assertEquals(lexTokenType("/"),
-        OxlangLexer.SYMBOL);
-
-    assertEquals(lexTokenType("+"),
-        OxlangLexer.SYMBOL);
-
-    assertEquals(lexTokenType("-"),
-        OxlangLexer.SYMBOL);
+    for (String s: examples)
+      assertToken(s, OxlangLexer.SYMBOL);
   }
 
   @Test
   public void testLexInteger() throws Exception {
-    assertEquals(lexTokenType("1"),
-        OxlangLexer.INTEGER);
+    String[] examples = {"1", "-2", "100_000_000"};
 
-    assertEquals(lexTokenType("-2"),
-        OxlangLexer.INTEGER);
-
-    assertEquals(lexTokenType("100_000_000"),
-        OxlangLexer.INTEGER);
+    for (String s: examples)
+      assertToken(s, OxlangLexer.SYMBOL);
   }
 
   @Test
   public void testLexFloat() throws Exception {
-    assertEquals(lexTokenType("1.1"),
-        OxlangLexer.FLOAT);
+    String[] examples = {"1.0", "1.1", "-1.2", "+1.3", "1e0", "-1e15", "2.0e-15"};
 
-    assertEquals(lexTokenType("-1.0"),
-        OxlangLexer.FLOAT);
-
-    assertEquals(lexTokenType("+3.14159"),
-        OxlangLexer.FLOAT);
-
-    assertEquals(lexTokenType("1e0"),
-        OxlangLexer.FLOAT);
-
-    assertEquals(lexTokenType("-1e0"),
-        OxlangLexer.FLOAT);
-
-    assertEquals(lexTokenType("1e0"),
-        OxlangLexer.FLOAT);
-
-    assertEquals( lexTokenType("2e-15"),
-        OxlangLexer.FLOAT);
+    for (String s : examples)
+      assertToken(s, OxlangLexer.FLOAT);
   }
 }
